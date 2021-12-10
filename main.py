b1ce5fa6c8713eb0c7d36fff432def01efb73c89
Frerik Nagel <57255358+Thepenguincoder@users.noreply.github.com>
2021-12-10 15:13:39 +0000

    Computer speelt nu de winnende move (als die er is

--- main.py
+++ main.py
@@ -12,13 +12,27 @@ class MyCapableAgent(EvaluationAgent):
       boardCopy.append(i)
     return boardCopy
 
-  
+  def isWinner(bo, le):
+    return ((bo[7] == le and bo[8] == le and bo[9] == le) or
+    (bo[4] == le and bo[5] == le and bo[6] == le) or
+    (bo[1] == le and bo[2] == le and bo[3] == le) or
+    (bo[7] == le and bo[4] == le and bo[1] == le) or
+    (bo[8] == le and bo[5] == le and bo[2] == le) or
+    (bo[9] == le and bo[6] == le and bo[3] == le) or
+    (bo[7] == le and bo[5] == le and bo[3] == le) or
+    (bo[9] == le and bo[5] == le and bo[1] == le))
   
   def evaluate(self, board, my_symbol, opponent_symbol):
-    getal = random.randint(1, 500)
-    if board[4] == None:
-      getal = 4
-    return getal
+    for i in range (0,9):
+      boardCopy = MyCapableAgent.getBoardCopy(board)
+      if boardCopy[i] != None:
+        boardCopy[i] = my_symbol
+        if MyCapableAgent.isWinner(boardCopy, my_symbol):
+          return i
+
+    return random.randint(0,8)
+
+
 
 #my_random_agent = MyRandomAgent()
 my_agent = MyCapableAgent()
