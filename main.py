6dfb799758a0ae866a90c751ce80db0e11ece097
Frerik Nagel <57255358+Thepenguincoder@users.noreply.github.com>
2021-12-10 15:36:02 +0000

    if the computer cannot win, it will now play on a random corner, or if that is also not an option, it plays on a random side (the random moves are selected from still valid moves

--- main.py
+++ main.py
@@ -13,20 +13,35 @@ class MyCapableAgent(EvaluationAgent):
     return boardCopy
   
   def evaluate(self, board, my_symbol, opponent_symbol):
-    for i in range (0,9):
+    for i in board:
       boardCopy = MyCapableAgent.getBoardCopy(board)
       if boardCopy[i] == None:
         boardCopy[i] = my_symbol
         if is_winner(boardCopy, my_symbol):
           return i
 
-    for i in range (0,9):
+    for i in board:
       boardCopy = MyCapableAgent.getBoardCopy(board)
       if boardCopy[i] == None or boardCopy[i] == '':
         boardCopy[i] = opponent_symbol
         if is_winner(boardCopy, opponent_symbol):
           return i
     
+    cornerMoves = [0,2,6,8]
+    possibleMoves = []
+    for i in cornerMoves:
+      if board[i] == None or board[i] == '':
+        possibleMoves.append(i)
+        if len(possibleMoves) != 0:
+          return random.choice(possibleMoves)
+
+    sideMoves = [1,3,5,7]
+    possibleMoves = []
+    for i in sideMoves:
+      if board[i] == None or board[i] == '':
+        possibleMoves.append(i)
+        if len(possibleMoves) != 0:
+          return random.choice(possibleMoves)
 
     return random.randint(0,8)
 
