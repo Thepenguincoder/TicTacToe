cb3706070122e5adc526a2fb03503219cfef8af8
Frerik Nagel <57255358+Thepenguincoder@users.noreply.github.com>
2021-12-10 10:44:26 +0000

    two computers to play against each other

--- main.py
+++ main.py
@@ -2,16 +2,14 @@ import random
 from bke import EvaluationAgent, start
 
 class MyRandomAgent(EvaluationAgent):
-    def evaluate(self, board, my_symbol, opponent_symbol):
-        return random.randint(1, 500)
+  def evaluate(self, board, my_symbol, opponent_symbol):
+    return random.randint(1, 500)
 
 class MyCapableAgent(EvaluationAgent):
-    def evaluate(self, board, my_symbol, opponent_symbol):
-        getal = random.randint(1, 500)
-        if board[4] == None:
-            getal = 4
-        return getal
+  def evaluate(self, board, my_symbol, opponent_symbol):
+    return random.randint(1, 500)
 
 my_random_agent = MyRandomAgent()
 my_agent = MyCapableAgent()
-start(player_x = my_random_agent, player_o = my_agent)
+start(player_o = my_random_agent)
+
