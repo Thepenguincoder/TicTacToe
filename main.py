c5627c4c3d21656b36d8a46734f9625f7f0f205d
Frerik Nagel <57255358+Thepenguincoder@users.noreply.github.com>
2021-12-18 14:55:35 +0000

    de verschillende keuzes staan nu in aparte functies

--- main.py
+++ main.py
@@ -17,11 +17,84 @@ def parameters():
   print("uitleg hyperparameter enzo, tussen 1 en 0")
   chosen_alpha = float(input("alpha: \n"))
   chosen_epsilon = float(input("epsilon: \n"))
-  #global my_agent
   my_agent = MyAgent(alpha=chosen_alpha, epsilon=chosen_epsilon)
   return my_agent
 
 
+
+def choiche2():
+  play_agent = input("Tegen welke agent wil je spelen? Als je tegen de agent van dit programma wilt spelen, vul in: agent1 \n")
+
+  my_agent = load(play_agent)
+ 
+  my_agent.learning = False
+
+  play_symbol = input("Wil je x of o zijn? (x begint altijd) \n")
+  if play_symbol == "x" or "X":
+    start(player_o=my_agent)  
+  elif play_symbol == "o" or "O":
+    start(player_x=my_agent)
+
+
+def choice3():
+  name = input("Hoe wil je dat je agent heet? \n")
+
+  while True:
+    training = int(input("hoe vaak moet je agent getraint worden? (max 50000) \n"))
+    if training <= 50000:
+      break
+
+  print("wil je de hyperparameters aanpassen? (y/n)")
+  if input() == "y":
+    my_agent = parameters()
+  else:
+    my_agent = MyAgent()
+
+  train(my_agent, training)
+ 
+  save(my_agent, name)
+
+  print("\nJe agent is getrain en gesaven onder: " + name)
+
+
+def choice4():
+  print("Als de grafiek getekend is, klik het weg om verder te gaan")
+
+  name = input("Hoe heet de agent \n")
+  symbol = input("Wil je dat jouw agent x of o is? (x begint altijd) \n")
+
+  my_agent = load(name)
+  my_agent.learning = False
+ 
+  validation_agent = RandomAgent()
+
+  if symbol == "x" or symbol == "X":
+    validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
+
+  if symbol == "o" or symbol == "O":
+    validation_result = validate(agent_o=my_agent, agent_x=validation_agent, iterations=100)
+
+  plot_validation(validation_result)
+
+def choice5():
+  print("Als de grafiek getekend is, klik het weg om verder te gaan")
+
+  random.seed(1)
+
+  while True:
+    itterate = int(input("How many itterations doe you want to do (max 50) \n"))
+    if itterate <= 50:
+      break
+
+  my_agent = parameters()
+  random_agent = RandomAgent()
+ 
+  train_and_plot(
+    agent=my_agent,
+    validation_agent=random_agent,
+    iterations=itterate,
+    trainings=100,
+    validations=1000)
 
 
 #1: 2 spelers
@@ -33,93 +106,20 @@ def parameters():
 while True:
   choice = input("\n 1: 2 spelers \n 2: speel tegen een getrainde computer \n 3: Train een AI \n 4: check hoe goed een bepaalde agent is \n 5: testen hoe goed bepaalde hyperparameter combinaties werken \n Kies wat je wilt spelen: \n")
 
-
   if choice == '1':
     start()
 
-
-
   if choice == '2':
-    play_agent = input("Tegen welke agent wil je spelen? Als je tegen de agent van dit programma wilt spelen, vul in: agent1 \n")
-
-    my_agent = load(play_agent)
- 
-    my_agent.learning = False
-
-    play_symbol = input("Wil je x of o zijn? (x begint altijd) \n")
-    if play_symbol == "x" or "X":
-      start(player_o=my_agent)  
-    elif play_symbol == "o" or "O":
-      start(player_x=my_agent)  
-
-
-
+    choiche2()
 
   if choice == '3':
-    name = input("Hoe wil je dat je agent heet? \n")
-
-    while True:
-      training = int(input("hoe vaak moet je agent getraint worden? (max 50000) \n"))
-      if training <= 50000:
-        break
-
-    print("wil je de hyperparameters aanpassen? (y/n)")
-    if input() == "y":
-      my_agent = parameters()
-    else:
-      my_agent = MyAgent()
-
-    train(my_agent, training)
- 
-    save(my_agent, name)
-
-    print("\nJe agent is getrain en gesaven onder: " + name)
-
-
-
-
+    choice3()
 
   if choice == '4':
-    print("Als de grafiek getekend is, klik het weg om verder te gaan")
-
-    name = input("Hoe heet de agent \n")
-    symbol = input("Wil je dat jouw agent x of o is? (x begint altijd) \n")
-
-    my_agent = load(name)
-    my_agent.learning = False
- 
-    validation_agent = RandomAgent()
-
-    if symbol == "x" or symbol == "X":
-      validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
-
-    if symbol == "o" or symbol == "O":
-      validation_result = validate(agent_o=my_agent, agent_x=validation_agent, iterations=100)
-
-    plot_validation(validation_result)
-
-
-
+    choice4()
 
   if choice == "5":
-    print("Als de grafiek getekend is, klik het weg om verder te gaan")
-
-    random.seed(1)
-
-    while True:
-      itterate = int(input("How many itterations doe you want to do (max 50) \n"))
-      if itterate <= 50:
-        break
-
-    my_agent = parameters()
-    random_agent = RandomAgent()
- 
-    train_and_plot(
-      agent=my_agent,
-      validation_agent=random_agent,
-      iterations=itterate,
-      trainings=100,
-      validations=1000)
+    choice5()
 
   play = input("Wil je doorgaan? (y/n) \n")
   if play == 'y':
